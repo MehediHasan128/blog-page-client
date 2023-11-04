7bd57c2324ec64e147484b7e674ffe2a4103ba39
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-04 19:50:14 +0530

    besic setup complete

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,11 @@
+import {createBrowserRouter} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Hello everyone</h1>
+    }
+]);
+
+
+export default router;
